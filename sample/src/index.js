8d5e1dc4ad05a915f1e7e33301162262e8956960
sorniak <dsh@corcode.com>
2020-09-05 22:20:49 +0200

    Wrap up

--- sample/src/index.js
+++ sample/src/index.js
@@ -1,1 +1,75 @@
-export default ()=> 10;
+(function main() {
+  const newDiv = document.createElement("div");
+  newDiv.className = "sorniaks_converter";
+  document.body.appendChild(newDiv);
+  item = document.getElementsByClassName("sorniaks_converter")[document.getElementsByClassName("sorniaks_converter").length - 1];
+  item.appendChild(document.createElement("input")).placeholder = "Source numeral system";
+  item.appendChild(document.createElement("input")).placeholder = "Target numeral system";
+  item.appendChild(document.createElement("input")).placeholder = "Number to be converted";
+  item.appendChild(document.createElement("input")).placeholder = "Result";
+  item.appendChild(document.createElement("button")).innerText = "Convert!";
+
+  document.querySelectorAll(".sorniaks_converter button")[document.querySelectorAll(".sorniaks_converter button")
+    .length - 1]
+    .addEventListener('click', function (event) {
+      this.previousElementSibling.value = convertBase(
+        this.previousElementSibling.previousElementSibling.value,
+        this.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.value,
+        this.previousElementSibling.previousElementSibling.previousElementSibling.value,
+        event.clientX,
+        event.clientY
+      );
+    });
+  return newDiv;
+
+  function convertBase(value, from_base, to_base, x, y) {
+    if ((value !== '') && (from_base !== '') && (to_base !== '')) {
+      var range = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/'.split('');
+      var from_range = range.slice(0, from_base);
+      var to_range = range.slice(0, to_base);
+
+      var dec_value = value.split('').reverse().reduce(function (carry, digit, index) {
+        if (from_range.indexOf(digit) === -1) {
+          const errMsg = 'Invalid digit `' + digit + '` for base ' + from_base + '.';
+          popUp(errMsg, x, y);
+        }
+        return carry += from_range.indexOf(digit) * (Math.pow(from_base, index));
+      }, 0);
+
+      var new_value = '';
+      while (dec_value > 0) {
+        new_value = to_range[dec_value % to_base] + new_value;
+        dec_value = (dec_value - (dec_value % to_base)) / to_base;
+      }
+      return new_value || '0';
+    } else {
+      const errMsg = 'Empty input field(s)!';
+      popUp(errMsg, x, y);
+    }
+  }
+
+  function popUp(messageText, x, y) {
+    var popup = document.createElement('div');
+    popup.className = "sorniaks_converter_error";
+    var cancel = document.createElement('div');
+    cancel.innerHTML = 'X';
+    cancel.style.float = "right";
+    cancel.style.width = "fit-content";
+    cancel.style.cursor = "pointer";
+    cancel.style.border = "solid 2px black"
+    popup.style.color = "white";
+    popup.style.fontSize = "2vw";
+    cancel.onclick = function (e) { popup.parentNode.removeChild(popup) };
+    var message = document.createElement('span');
+    message.style.margin = "0 5px 0 5px";
+    message.innerHTML = messageText;
+    popup.appendChild(message);
+    popup.appendChild(cancel);
+    popup.style.position = "absolute";
+    popup.style.width = "fit-content";
+    popup.style.top = y + "px";
+    popup.style.left = x + "px";
+    popup.style.backgroundColor = "red";
+    document.body.appendChild(popup);
+  }
+})();
--- sample/tests/spec_a.js
+++ sample/tests/spec_a.js
@@ -1,7 +1,52 @@
-import myModule from './../src/index.js';
+require('./../src/index.js');
+delete require.cache[require.resolve('./../src/index.js')]; // twice to run, second import makes new widget
+require('./../src/index.js');
 
-describe("Module should return", function () {
-  it("some number", function () {
-    expect(myModule()).toEqual(10);
+describe("converter tetsting", function () {
+  beforeAll(() => {
+
+    // first widget
+    document.querySelectorAll("div.sorniaks_converter :nth-child(1)")[0].setAttribute('value', '10'); // source numerical system
+    document.querySelectorAll("div.sorniaks_converter :nth-child(2)")[0].setAttribute('value', '7'); // target numerical system
+    document.querySelectorAll("div.sorniaks_converter :nth-child(3)")[0].setAttribute('value', '120'); // number to be converted
+
+    // second widget
+    document.querySelectorAll("div.sorniaks_converter :nth-child(1)")[1].setAttribute('value', '1'); // source numerical system
+    document.querySelectorAll("div.sorniaks_converter :nth-child(2)")[1].setAttribute('value', '2'); // target numerical system
+    document.querySelectorAll("div.sorniaks_converter :nth-child(3)")[1].setAttribute('value', '3'); // number to be converted
   });
+
+  it("generates div element", () => {
+    expect(document.querySelector("div.sorniaks_converter")).toBeTruthy();
+  });
+
+  it("generates button", () => {
+    expect(document.querySelector("div.sorniaks_converter :nth-child(5)").nodeName).toBe("BUTTON");
+  });
+
+  it("generates four inputs, 3 for input, one for output", () => {
+    expect(document.querySelector("div.sorniaks_converter :nth-child(1)").nodeName).toBe("INPUT");
+    expect(document.querySelector("div.sorniaks_converter :nth-child(2)").nodeName).toBe("INPUT");
+    expect(document.querySelector("div.sorniaks_converter :nth-child(3)").nodeName).toBe("INPUT");
+    expect(document.querySelector("div.sorniaks_converter :nth-child(4)").nodeName).toBe("INPUT");
+  });
+
+  it("converts properly on test case", () => {
+    document.querySelectorAll("div.sorniaks_converter :nth-child(5)")[0].click();
+    expect(document.querySelectorAll("div.sorniaks_converter :nth-child(4)")[0].value).toEqual('231');
+  });
+
+  it("shows invalid digit error and error note can be closed", () => {
+    document.querySelectorAll("div.sorniaks_converter :nth-child(5)")[1].click();
+    expect(document.querySelector("div.sorniaks_converter_error")).toBeTruthy();
+    document.querySelector("div.sorniaks_converter_error div").click();
+    expect(document.querySelector("div.sorniaks_converter_error")).toBeFalsy();
+  });
+
+  it("shows empty input fields error", () => {
+    document.querySelectorAll("div.sorniaks_converter :nth-child(3)")[1].setAttribute('value', '');
+    document.querySelectorAll("div.sorniaks_converter :nth-child(5)")[1].click();
+    expect(document.querySelector("div.sorniaks_converter_error")).toBeTruthy();
+  });
+
 });
